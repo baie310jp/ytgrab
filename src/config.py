44eef6777f42a0a837e0b51ff89e1715ff84da28
baie310jp <baie310jp@gmail.com>
2026-05-16 19:12:29 +0900

    update

--- src/config.py
+++ src/config.py
@@ -11,7 +11,7 @@ ydl_opts_mp3 = {
 }
 
 ydl_opts_mp4 = {
-    "format": "bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720][ext=mp4]/best[height<=720]/best",
+    "format": "bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720][ext=mp4]",
     "merge_output_format": "mp4",
     "outtmpl": None,
 }

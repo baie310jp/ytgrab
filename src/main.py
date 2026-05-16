8e26c76cf352f6c234b40366ce4a96da5340ec58
baie310jp <baie310jp@gmail.com>
2026-05-16 15:34:10 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
+import os
+
+from yt_dlp import YoutubeDL
+
 from videos import videos
 
 
-def main():
+def main(format="mp3"):
     for video in videos:
-        print(video)
+        vid = video["id"]
+        title = video["title"]
+        artist = video["artist"]
+
+        if format == "mp3":
+            output_vid = f"output/mp3/{vid}"
+            file_path = output_vid + ".mp3"
+        else:
+            output_vid = f"output/mp4/{vid}"
+            file_path = output_vid + ".mp4"
+
+        if os.path.isfile(file_path):
+            continue
 
 
 if __name__ == "__main__":

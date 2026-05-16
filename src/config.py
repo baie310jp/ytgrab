be743137cc7181fdcd2ac3aceeb8659c7d13ec67
baie310jp <baie310jp@gmail.com>
2026-05-16 15:50:27 +0900

    update

---
+++ src/config.py
@@ -0,0 +1,13 @@
+ydl_opts_mp3 = {
+    "format": "bestaudio/best",
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
+        }
+    ],
+    "outtmpl": None,
+}
+
+ydl_opts_mp4 = {}
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 
 from yt_dlp import YoutubeDL
 
+from config import ydl_opts_mp3, ydl_opts_mp4
 from videos import videos
 
 
@@ -14,15 +15,23 @@ def main(format="mp3"):
         if format == "mp3":
             output_vid = f"output/mp3/{vid}"
             file_path = output_vid + ".mp3"
+            ydl_opts = ydl_opts_mp3
+            ydl_opts["outtmpl"] = output_vid
         else:
             output_vid = f"output/mp4/{vid}"
             file_path = output_vid + ".mp4"
+            ydl_opts = ydl_opts_mp4
+            ydl_opts["outtmpl"] = output_vid
 
         if os.path.isfile(file_path):
             continue
 
         url = f"https://i.ytimg.com/vi/{vid}/hqdefault.jpg"
-        print(url)
+
+        with YoutubeDL(ydl_opts) as ydl:
+            retcode = ydl.download([f"https://www.youtube.com/watch?v={vid}"])
+            if retcode != 0:
+                raise RuntimeError(f"Downloading failed with non-zero return code. ({vid})")
 
 
 if __name__ == "__main__":
